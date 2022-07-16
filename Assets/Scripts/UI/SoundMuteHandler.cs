670baa62c86aed05cd84e8252279cb17649ceac4
Daehico <empire_95@mail.ru>
2022-07-16 03:55:40 +0300

    resolve sound mute/unmute problem

--- Assets/Scripts/UI/SoundMuteHandler.cs
+++ Assets/Scripts/UI/SoundMuteHandler.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using Agava.YandexGames.Utility;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,6 +11,28 @@ public class SoundMuteHandler : MonoBehaviour
 
     private bool _isSoundMute = false;
 
+    private void Start()
+    {
+        Debug.Log(_isSoundMute);
+        _isSoundMute = Convert.ToBoolean(PlayerPrefs.GetInt("SoundMute"));
+        Debug.Log(_isSoundMute);
+
+        if (_isSoundMute == true)
+        {
+            AudioListener.pause = true;
+            AudioListener.volume = 0f;
+            _image.texture = _mute;
+        }
+        else
+        {
+            AudioListener.pause = false;
+            AudioListener.volume = 0.5f;
+            _image.texture = _unmute;
+
+        }
+
+    }
+
     public void SoundMuteButtonOn()
     {
         if (_isSoundMute == false)
@@ -18,12 +40,25 @@ public class SoundMuteHandler : MonoBehaviour
             AudioListener.pause = true;
             _isSoundMute = true;
             _image.texture = _mute;
+            AudioListener.volume = 0f;
+            PlayerPrefs.SetInt("SoundMute", Convert.ToInt32(_isSoundMute));
+            Debug.Log(_isSoundMute);
         }
         else
         {
             AudioListener.pause = false;
             _isSoundMute = false;
             _image.texture = _unmute;
+            PlayerPrefs.SetInt("SoundMute", Convert.ToInt32(_isSoundMute));
+            AudioListener.volume = 0.5f;
+            Debug.Log(_isSoundMute);
         }
+    }
+
+    private void Update()
+    {
+        if (_isSoundMute == true)
+            return;
+        AudioListener.pause = WebApplication.InBackground;
     }
 }
